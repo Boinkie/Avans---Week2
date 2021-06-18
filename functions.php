a0034a9fcf6ea2d11ab2aaaa187b1f473fb866d6
Boinkie <spamrentier@gmail.com>
2021-06-18 12:37:53 +0200

    nieuwe functie toegevoegd sessionCount

--- functions.php
+++ functions.php
@@ -1,5 +1,6 @@
 <?php 
 
+// functie om een email te versturen met $_POST data
 function sendMail ($input){
     $adminEmail = $input['email'];
     $message = $input['message'];
@@ -10,4 +11,18 @@ function sendMail ($input){
     } else {
         echo "<p>Mail niet verzonden</p>";
     }
+}
+
+// Functie om aantal bezoeken te tellen met session
+function sessionCount ($input) {
+if (!isset($input)) {
+    $input = 0;
+}
+$visits = ++$input;
+setcookie('count', $visits , time() + 2592000 , "/");
+if ($visits > 1) {
+    echo("<p>Je hebt deze pagina $visits keer bezocht!</p>");
+} else { // First visit
+    echo('<p>Je hebt deze website nog niet eerder bezocht, welkom!</p>');
+}
 }
